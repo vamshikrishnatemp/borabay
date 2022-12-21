fceb48d21f87778acac9145cf3d803ac1da9f9cb
vamshikrishnatemp <vamshikrishnatemp@gmail.com>
2022-12-21 20:07:44 +0530

    home page responsiveness done

--- pages/home/index.js
+++ pages/home/index.js
@@ -5,7 +5,7 @@ import { Modal } from '../../src/components/modal';
 function Home(props) {
     const [openMenu, setOpenMenu] = useState(false);
     return (
-        <div className={`relative w-screen h-screen flex flex-col justify-center items-center bg-borabayLightBrown`} id='home' >
+        <div className={`relative w-screen h-screen flex flex-col pt-[70px] sm:pt-0 sm:justify-center items-center bg-borabayLightBrown`} id='home' >
             <div className='absolute w-[60px] h-[45px] top-5 left-5 flex justify-center items-center lg:hidden'>
                 <Image className='w-full h-full' src={require('../../public/icons/Logo.png')} alt='Logo' />
             </div>
@@ -13,22 +13,25 @@ function Home(props) {
                 <Image src={require('../../public/icons/ham-menu.png')} height={25} width={40} alt='ham-menu' />
             </button>
             {openMenu && <Modal onCloseClick={setOpenMenu} />}
-            <div className='pt-[50px] w-[400px] flex flex-col justify-center self-end md:self-center items-center gap-3'>
-                <span className={`py-3 text-borabayDarkBrown font-normal text-2xl font-montserrat`}>Experience Beauty In-Depth  at</span>
-                <span className={`text-borabayBrown font-normal text-8xl font-higuen`}>Borabay</span>
-                <span className={`text-borabayBrown text-2xl tracking-[.60em] font-hindguntur`}>THE SALON</span>
-                <span className={`py-3 text-borabayDarkBrown font-normal text-2xl text-center font-montserrat`}>Where nature and science interact to concept encompassing beauty solutions</span>
-                <div className='pt-[100px] hidden md:flex flex-col items-center justify-center gap-2'>
+            <div className='w-[70%] sm:w-[400px] flex flex-col sm:mr-[8%] lg:mr-0 justify-center sm:self-end lg:self-center items-center gap-3'>
+                <span className={`py-3 text-borabayDarkBrown font-normal text-xl sm:text-2xl text-center font-montserrat`}>Experience Beauty In-Depth  at</span>
+                <span className={`text-borabayBrown font-normal text-4xl sm:text-8xl font-higuen`}>Borabay</span>
+                <span className={`text-borabayBrown text-xl sm:text-2xl sm:tracking-[.60em] font-hindguntur`}>THE SALON</span>
+                <span className={`py-3 text-borabayDarkBrown font-normal text-xl sm:text-2xl text-center font-montserrat`}>Where nature and science interact to concept encompassing beauty solutions</span>
+                <div className='pt-[100px] hidden lg:flex flex-col items-center justify-center gap-2'>
                     <span className={`text-borabayBrown font-normal text-2xl font-montserrat`}>Scroll</span>
                     <Image src={require('../../public/icons/down_arrow.png')} height={70} width={70} alt='Down-Arrow' />
                 </div>
             </div>
-            <div className='absolute h-[60%] w-[40%] left-0 flex items-end bottom-0'>
-                <Image src={require('../../public/icons/green-leafed-girl.png')} height={600} width={380} alt='green-leafed-girl' />
+            <div className='absolute left-0 bottom-0 h-[80%] w-[60%] hidden sm:flex items-end'>
+                <Image src={require('../../public/icons/green-leafed-girl.png')} height={700} width={380} alt='green-leafed-girl' />
                 {/* <img src='../../public/icons/green-leafed-girl.png' style={{ height: '100px', width: '100px' }} alt='green-leafed-girl' /> */}
             </div>
             <div className='absolute right-0 -bottom-[0px] hidden lg:flex'>
-                <Image src={require('../../public/icons/white-top-girl.png')} height={700} width={380} alt='white-top-girl' />
+                <Image src={require('../../public/icons/white-top-girl.png')} height={750} width={370} alt='white-top-girl' />
+            </div>
+            <div className='absolute -bottom-[5px] sm:hidden'>
+                <Image src={require('../../public/icons/white-top-girl-uncroped.png')} height={400} width={270} alt='white-top-girl' />
             </div>
         </div >
     );
